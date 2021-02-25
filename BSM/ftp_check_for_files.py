e7f482af68f8cc9e4eb84a3f89e2f742b47b071c
imjacksdad <imjacksdad@gmail.com>
2021-02-25 15:12:15 -0600

    got bsm sftp working

--- BSM/ftp_check_for_files.py
+++ BSM/ftp_check_for_files.py
@@ -6,7 +6,7 @@ import shutil
 
 
 ftps = FTP('ftp.buysellmove.com')  # connect to host, default port
-ftps.login(user='gavilon',passwd='gAv1l0n%%2Fcntr%%2F020620')
+ftps.login(user='gavilon',passwd='gAv1l0n/cntr/020620')
 
 interval = .5  # in seconds.  Can be a decimal
 
@@ -17,14 +17,14 @@ ftps.pwd()
 # Get All Files
 files = ftps.nlst()
 fn = 0
-##for file in files:
-###    time.sleep(interval)
-##    try:
-##        #print("Found..." + file)
-##        fn += 1
-##        #ftps.retrbinary("RETR " + file ,open(file, 'wb').write)
-##        ##ftp_get_file(file)
-##    except:
-##        print ("Error: File could not be downloaded or found " + file)
+for file in files:
+   time.sleep(interval)
+   try:
+       print("Found..." + file)
+       fn += 1
+        #ftps.retrbinary("RETR " + file ,open(file, 'wb').write)
+        ##ftp_get_file(file)
+   except:
+        print ("Error: File could not be downloaded or found " + file)
 ftps.quit()
 
