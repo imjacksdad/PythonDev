7483ca8896446a53d8d7baae3e557f2c9c9768e3
jacksdad <imjacksdad@gmail.com>
2021-05-19 21:03:30 -0500

    twitter_scapper

---
+++ Twitter_Scapper/twitter_scrapper.py
@@ -0,0 +1,45 @@
+import tweepy
+from tweepy import OAuthHandler
+import pandas as pd
+
+"""I like to have my python script print a message at the beginning. This helps me confirm whether everything is set up correctly. And it's nice to get an uplifting message ;)."""
+
+print("You got this!")
+
+access_token = '2416133377-lebxYOI7QQaEYVTMKPGeh5Llr6rEeZ6p7b4DLtf'
+access_token_secret = 'TkhDh7W3hIFgpIL0QXdmplNDZguBKq3H9oVFjVeMRI3PZ'
+consumer_key = 'N01MEjB95ekP0tGL88XjdPO0X'
+consumer_secret = '8Jt4hIV66Eud07C02IVvP0tFCw8hQ5R83kODhptrOZFQbs0GWX'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+tweets = []
+
+count = 1
+
+"""Twitter will automatically sample the last 7 days of data. Depending on how many total tweets there are with the specific hashtag, keyword, handle, or key phrase that you are looking for, you can set the date back further by adding since= as one of the parameters. You can also manually add in the number of tweets you want to get back in the items() section."""
+
+for tweet in tweepy.Cursor(api.search, q="@ActingTheGom", count=450, since='2020-02-28').items(5000):
+	
+	print(count)
+	count += 1
+
+	try: 
+		data = [tweet.created_at, tweet.id, tweet.text, tweet.user._json['screen_name'], tweet.user._json['name'], tweet.user._json['created_at'], tweet.entities['urls']]
+		data = tuple(data)
+		tweets.append(data)
+
+	except tweepy.TweepError as e:
+		print(e.reason)
+		continue
+
+	except StopIteration:
+		break
+
+df = pd.DataFrame(tweets, columns = ['created_at','tweet_id', 'tweet_text', 'screen_name', 'name', 'account_creation_date', 'urls'])
+
+"""Add the path to the folder you want to save the CSV file in as well as what you want the CSV file to be named inside the single quotations"""
+df.to_csv(path_or_buf = 'C:/Python39/PythonDev/Twitter_Scapper/FileName.csv', index=False) 
---
+++ VariousSnippets/pnl.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pylab as plt
+
+# Let's create an arbitrary price series from normally distributed random numbers. 
+# Of course, you can replace this with your downloaded historical data. 
+np.random.seed(28)
+N = 300
+rets = np.random.randn(N)
+prices = np.cumsum(rets)+10
+plt.plot(prices);
+plt.ylabel('Prices');
+
+# Current position. '0': not in position. '1': in long position. '-1': in short position
+inpos = 0
+
+# Positions
+pos = []
+
+# Prices 
+px = []
+
+# Realised PnL
+rlzd = []
+
+# Exit times
+t = [] 
+
+# Moving Average windows
+MA = [3, 9]
+
+for i in range(max(MA)+1, N):
+    # The Moving Averages:
+    ma1 = np.mean(prices[i-MA[0]:i])
+    ma2 = np.mean(prices[i-MA[1]:i])
+    
+    pos.append(inpos)
+    px.append(prices[i])
+
+    if ma1 > ma2:
+        if inpos != -1: # If we are not in a short position
+            if inpos: # If we are in a long position, now we exit:
+                rlzd.append(prices[i]-entry)
+                # Record the exit times
+                t.append(i)
+            
+            # Enter a short position
+            entry = prices[i]
+            inpos = -1
+
+    else:
+        if inpos != 1: # If we are not in a long position    
+            if inpos:
+                rlzd.append(entry-prices[i])
+                t.append(i)
+
+            # Enter a long position
+            entry = prices[i]
+            inpos = 1
+
+# Realised PnL
+plt.plot(np.array(t)-max(MA)-2, np.cumsum(rlzd), '-o');
+
+# Unrealised PnL
+unrlzd = np.cumsum(np.multiply(np.diff(px), pos[1:]))
+
+# Compare the final realised and the final unrealised PnL:
+final_unrlzd = unrlzd[-1]
+print(final_unrlzd, sum(rlzd))
+
+# Compare the realised and unrealised PnLs:
+plt.plot(np.array(t)-max(MA)-2, np.cumsum(rlzd), '-o')
+plt.plot(unrlzd,'--')
+plt.title('Realised vs unrealised PnLs')
+plt.legend(['realised', 'unrealised']);
+
+# To zoom in:
+#plt.xlim([20,50])
+#plt.ylim([-15,-5])
+
+
+                
