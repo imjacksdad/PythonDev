235ff0cee7caf977ac3e6f3a704f75346f564c62
jacksdad <imjacksdad@gmail.com>
2021-06-08 21:44:07 -0500

    restore

--- Wendi code tries/ec_web_scrape.py
+++ Wendi code tries/ec_web_scrape.py
@@ -2,13 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = 'https://www.census.gov/programs-surveys/popest.html'
+
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-#results = soup.find(id='ResultsContainer')
+job_elems = results.find_all('section', class_='card-content')
 
-#print(results.prettify())
-
+for job_elem in job_elems:
+    print(job_elem, end='\n'*2)
 
 
