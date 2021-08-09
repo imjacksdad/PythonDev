144c25966b00ca34d4984ca21dc832d0247e40e6
imjacksdad <imjacksdad@gmail.com>
2021-08-09 15:14:57 -0500

    Create swap 2 variables.py

---
+++ VariousSnippets/swap 2 variables.py
@@ -0,0 +1,49 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Python39/swap tow variables.py ==================
+10
+5
+>>> 
+================== RESTART: C:/Python39/swap tow variables.py ==================
+10
+5
+15
+>>> 
+================== RESTART: C:/Python39/swap tow variables.py ==================
+10
+5
+15
+10
+>>> 
+>>> 
+================== RESTART: C:/Python39/swap tow variables.py ==================
+10
+5
+-5
+10
+>>> 
+================== RESTART: C:/Python39/swap tow variables.py ==================
+10
+5
+15
+10
+-5
+10
+>>> 
+================== RESTART: C:/Python39/swap tow variables.py ==================
+10
+5
+15
+10
+5
+10
+>>> 
+================== RESTART: C:/Python39/swap tow variables.py ==================
+10
+5
+15
+10
+5
+10
+>>> 
