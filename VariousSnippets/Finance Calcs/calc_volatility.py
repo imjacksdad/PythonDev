e2279576e0c380bf8729983cfd151efb6b0c2a3f
imjacksdad <imjacksdad@gmail.com>
2021-12-28 10:03:47 -0600

    calc volatility

--- VariousSnippets/Finance Calcs/calc_volatility.py
+++ VariousSnippets/Finance Calcs/calc_volatility.py
@@ -1,18 +1,18 @@
 ##syntax to write the function to calculate implied volatility for Call Option and Put Option would be —
-##mibian.BS([Underlying Price, Call / Price Strike Price, Interest Rate, Days To Expiration], Call / Put Price)
+##mibian.BS([Underlying Price, Call Price or Strike Price, Interest Rate, Days To Expiration], Call / Put Price)
 
 # Contract # = '1C03 DV0E0
 import mibian
 
-c = mibian.BS([6.54, 6, .001, 64], callPrice= .78)
+c = mibian.BS([0, 0, .001, 26], callPrice= 6.2)
 print('Call Vol: ' + str(c.impliedVolatility))
 print()
 
-p = mibian.BS([6.54, 6, .001, 64], callPrice= .78)
+p = mibian.BS([6.025, 6.2, .001, 26], callPrice= 6.2)
 print('Put Vol: ' + str(p.impliedVolatility))
 print()      
 
-c = mibian.BS([6.54, 6, .001, 64], volatility = 43.9453125)
+c = mibian.BS([6.025, 6.2, .001, 26], volatility = 500)
 print('Call Price: ' + str(c.callPrice))
 print('Call Delta: ' + str(c.callDelta))
 print('Call Theta: ' + str(c.callTheta))
@@ -21,7 +21,7 @@ print('Call Gamma: ' + str(c.gamma))
 print()
 print()
 
-p = mibian.BS([6.54, 6, .001, 64], volatility = 43.9453125)
+p = mibian.BS([6.025, 6.2, .001, 26], volatility = 500)
 print('Put Price: ' + str(p.callPrice))
 print('Put Delta: ' + str(p.callDelta))
 print('Put Theta: ' + str(p.callTheta))
---
+++ VariousSnippets/numpy test.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr =  np.array((12,20,30,40,50))
+print(arr)
